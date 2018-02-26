3670f06849dc20c2c69ef00dee547b3f415813af
Vitor Baggi de Almeida <vitor.baggi@aluno.ufabc.edu.br>
2018-02-26 15:13:41 -0300

    heroku config

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const port = 3000
+// const port = 3000
 var formidable = require('express-formidable'); //para tratar os parametros que chegam por post
 const app = express() //usando a biblioteca express para usar Router()
-app.use(formidable());
+app.set('port', (process.env.PORT || 5000));
+// app.use(formidable());
+app.use(express.static(__dirname + '/public'));
+
 let arduinoRoutes = require('./routes/arduinoRoutes')
 let mongo = require('./drivers/mongodb')
 
 app.use("/", arduinoRoutes)
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
+app.listen(app.get('port'), () => {
+    console.log(`Servidor rodando em http://localhost:${app.get('port')}`)
     console.log('Para derrubar o servidor: ctrl + c');
 })
