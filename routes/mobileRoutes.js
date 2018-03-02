d94e4c931bc097c0295643d5ac21aa53d51a23f3
Vitor Baggi de Almeida <vitor.baggi@aluno.ufabc.edu.br>
2018-03-02 15:52:47 -0300

    incluido bodyparser para funcoes posts

--- routes/mobileRoutes.js
+++ routes/mobileRoutes.js
@@ -3,7 +3,7 @@ var mobileController = require('../controllers/mobileController')
 
 
 router.post("/createPass", function(req, res){
-    mobileController.createPass(req.fields).then(response=>{
+    mobileController.createPass(req.body).then(response=>{
         res.status(200).send("OK")
     }).catch(error=>{
         res.status(500).send(error)
@@ -12,11 +12,16 @@ router.post("/createPass", function(req, res){
 
 router.post("/mobileLogin", function(req, res){
     //campos a passar 'user' e 'password' formato JSON
-    mobileController.login(req.fields).then(response=>{
+    mobileController.login(req.body).then(response=>{
         console.log(response)
         res.status(200).send(response)//deve retornar o TOKEN
     }).catch(error=>{
-        res.status(500).send(error)
+        console.log("erroo")
+        if(error.status == 401){
+            res.status(401).send(error.text)
+        }else{
+            res.status(500).send(error)
+        }
     })    
 })
 
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 // const port = 3000
 var formidable = require('express-formidable'); //para tratar os parametros que chegam por post
 const app = express() //usando a biblioteca express para usar Router()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.set('port', (process.env.PORT || 5000));
 // app.use(formidable());
 app.use(express.static(__dirname + '/public'));
